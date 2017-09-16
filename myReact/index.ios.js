0d2648a187d193af2bcc6e3eab48ea3b717a79a1
LuoTuxiu <359703836@qq.com>
2017-09-16 14:24:49 +0800

    registerComponent里面分号的词必须与项目名称一样

--- myReact/index.ios.js
+++ myReact/index.ios.js
@@ -4,29 +4,6 @@
  * @flow
  */
 
-// import React, { Component } from 'react';
-// import { AppRegistry, StyleSheet, Text, View } from 'react-native';
-//
-// class MyReact extends Component {
-//   render() {
-//     return (
-//       <View>
-//         <Text style={styles.red}>just red</Text>
-//       </View>
-//     )
-//   }
-// }
-//
-//
-// const styles  = StyleSheet.create({
-//   red: {
-//     color: 'red'
-//   }
-// })
-//
-// AppRegistry.registerComponent('MyReact', () => MyReact)
-
-
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
@@ -35,23 +12,46 @@ class MyReact extends Component {
     return (
       <View>
         <Text style={styles.red}>just red</Text>
-        <Text style={styles.bigblue}>just bigblue</Text>
-        <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
-        <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
       </View>
-    );
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  bigblue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
-  },
-  red: {
-    color: 'red',
-  },
-});
 
-AppRegistry.registerComponent('MyReact', () => MyReact);
+const styles  = StyleSheet.create({
+  red: {
+    color: 'red'
+  }
+})
+
+AppRegistry.registerComponent('myReact', () => MyReact)
+
+
+// import React, { Component } from 'react';
+// import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+//
+// class MyReact extends Component {
+//   render() {
+//     return (
+//       <View>
+//         <Text style={styles.red}>just red</Text>
+//         <Text style={styles.bigblue}>just bigblue</Text>
+//         <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+//         <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
+//       </View>
+//     );
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   bigblue: {
+//     color: 'blue',
+//     fontWeight: 'bold',
+//     fontSize: 30,
+//   },
+//   red: {
+//     color: 'red',
+//   },
+// });
+//
+// AppRegistry.registerComponent('myReact', () => MyReact);
